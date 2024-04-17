3e8079401d8b1d000904ecb101edf95c4caf42cf
laibamarrie <irshadirshad706@gmail.com>
2024-04-17 19:11:49 +0500

    Added Word Counter.

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+// Import the "inquirer" module and chalk library
+import inquirer from "inquirer";
+import chalk from "chalk";
+// Declare a constant "answers" and assign it to the result of inquirer.prompt function.
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the words: ",
+    },
+]);
+// Store the sentence in the sentenceArray
+const sentenceArray = answers.Sentence.trim().split(" ");
+// Print the array of words to console
+console.log(chalk.cyan("Your sentence words:"));
+console.log(chalk.gray(sentenceArray));
+// Print the word count of the sentence to the console
+console.log(chalk.green(`Your sentence word count is: ${sentenceArray.length}`));
